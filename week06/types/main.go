caa2d0a0b2d215d0b433b0dcf8d179e208eb3a47
HikiCrash <csh02127@naver.com>
2025-09-29 11:48:35 +0900

    v0.2 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
 	"strings"
 )
 
 func main() {
+	// var name string
+	// name = "Kim"
+
+	// var name = "Kim"
+
+	name := "Kim"
 	fmt.Println(math.Round(2.71))
 	fmt.Println(strings.Title("gohead first go"))
+	fmt.Println(name, reflect.TypeOf(name))
 }
